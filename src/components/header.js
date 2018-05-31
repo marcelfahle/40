914f177286345a63db359244efd8f6a7843fc5dd
Marcel Fahle <m.fahle@gmail.com>
2018-05-31 09:22:47 +0200

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = ({ siteTitle }) => (
+  <div
+    style={{
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    />
+  </div>
+)
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Liebe Famile und Freunde</h1>
+    <p>
+      Wie sicherlich bekannt ist, bin ich kein großer Fan von Geburtstagen,
+      insbesondere wenn die Aufmerksamkeit in meine Richtung schwingt.
+    </p>
+    <p>
+      Zudem vergesse ich auch die meisten Geburtstage die nicht meine sind
+      (Sorry, Tina. Sorry, Lisa).
+    </p>
+    <p>
+      Jetzt wo ich 40 werde, scheint es aber so, dass ich mich dieses Jahr nicht
+      dr&uuml;cken kann, und daher habe ich mich dazu entschlossen, mitzumachen
+      und wohlwollend jedesmal auf die Frage "was w&uuml;nscht Du Dir denn?" zu
+      antworten.
+    </p>
+    <p>
+      Der Einfachheit halber, habe ich eine kleine Liste erstellt, die Dinge
+      enth&auml;lt &uuml;ber die ich mich ehrlich und von ganzem Herzen freuen
+      w&uuml;rde.<br />
+    </p>
+    <p>
+      Die Liste findet Ihr hier und wird st&auml;ndig aktualisiert:{' '}
+      <a href="https://bit.ly/marcel40" target="_blank">
+        bit.ly/marcel40
+      </a>
+    </p>
+
+    <p>
+      (Ich habe Amazon Spanien benutzt, weil die meine Lieferadresse haben, aber
+      die meisten Artikel gibt's sicherlich in zig anderen Gesch&auml;ften. Dann
+      bitte nur darauf achten, die englischen Originale zu nehmen. Oft habe ich
+      mir auch bei den Editionen, z.b. Penguin Classics, etwas gedacht.)
+    </p>
+  </div>
+)
+
+export default IndexPage
