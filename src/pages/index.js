d4c4de7cb21f77213f706f8b3443a71db1d29f5b
Marcel Fahle <m.fahle@gmail.com>
2018-05-31 16:39:22 +0200

    Content by THEFEELING

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,28 +3,32 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Liebe Famile und Freunde</h1>
+    <h1>Liebe Familie und Freunde...</h1>
     <p>
-      Wie sicherlich bekannt ist, bin ich kein großer Fan von Geburtstagen,
-      insbesondere wenn die Aufmerksamkeit in meine Richtung schwingt.
+      Wie sicherlich bekannt sein dürfte, bin ich kein großer Fan von zu großer
+      Aufmerksamkeit in Richtung meiner Person – insbesondere bei Geburtstagen,
+      ahem.
     </p>
     <p>
-      Zudem vergesse ich auch die meisten Geburtstage die nicht meine sind
-      (Sorry, Tina. Sorry, Lisa).
+      Dazu passt gut, dass ich auch die meisten Geburtstage anderer lieber
+      Menschen vergesse & verpasse, ahem. (Sorry: Tina. Sorry, Lisa... wird
+      fortgesetzt)
     </p>
     <p>
-      Jetzt wo ich 40 werde, scheint es aber so, dass ich mich dieses Jahr nicht
-      dr&uuml;cken kann, und daher habe ich mich dazu entschlossen, mitzumachen
-      und wohlwollend jedesmal auf die Frage "was w&uuml;nscht Du Dir denn?" zu
-      antworten.
+      Jetzt aber, da ich in diesem Jahr stolze 40 Lenze zählen werde, dämmert es
+      selbst mir, dass ich mich nicht länger vor wichtigen sozialen Konventionen
+      drücken kann – und habe mich in der mir angeborenen Bescheidenheit dazu
+      entschlossen, bei allem und jedem mitzumachen und jedes Mal auf die Frage
+      “Was wünschst Du Dir denn?” wohlwollend eine Antwort zu geben.
     </p>
     <p>
-      Der Einfachheit halber, habe ich eine kleine Liste erstellt, die Dinge
-      enth&auml;lt &uuml;ber die ich mich ehrlich und von ganzem Herzen freuen
-      w&uuml;rde.<br />
+      Der Einfachheit halber habe ich deshalb eine kleine Liste mit Dingen
+      erstellt, über die ich mich ehrlich und von ganzem Herzen freuen würde.
+      Das tue ich alles für Euch.
     </p>
     <p>
-      Die Liste findet Ihr hier und wird st&auml;ndig aktualisiert: <br />
+      Besagte Liste findet Ihr hier und sie wird ständig aktualisiert -
+      versprochen:<br />
       <br />
       <strong>
         <a href="https://bit.ly/marcel40" target="_blank">
@@ -35,11 +39,11 @@ const IndexPage = () => (
 
     <p>
       <small>
-        (Ich habe Amazon Spanien benutzt, weil die meine Lieferadresse haben,
-        aber die meisten Artikel gibt's sicherlich in zig anderen
-        Gesch&auml;ften. Dann bitte nur darauf achten, die englischen Originale
-        zu nehmen. Oft habe ich mir auch bei den Editionen, z.b. Penguin
-        Classics, etwas gedacht.)
+        (Ich habe Amazon Spanien angegeben, weil die meine Lieferadresse haben.
+        Aber die meisten Artikel gibt’s sicherlich in zig anderen Geschäften.
+        Dann bitte nur darauf achten, die englischen Originale zu nehmen. Oft
+        habe ich mir auch bei den Editionen, z. B. “Penguin Classics”, etwas
+        gedacht.)
       </small>
     </p>
   </div>
